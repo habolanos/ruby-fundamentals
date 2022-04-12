671a722166ce3673fbe17df10f0c6e3bbe922abc
HAROLD ADRIAN <harold.bolanos@syncromsp.com>
2022-04-11 21:52:54 -0500

    Rollout simple form

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :autos
   resources :books
   get 'hola', to: 'welcome#hello'
--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_022030) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_024456) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.integer "status"
@@ -41,6 +41,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_022030) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.text "about_me"
+    t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

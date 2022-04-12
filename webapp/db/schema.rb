69279099ab5c72f90b45df92a53bd1898392d892
HAROLD ADRIAN <harold.bolanos@syncromsp.com>
2022-04-11 21:27:10 -0500

    Update db/schema.rb

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_020640) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_022030) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "autos", force: :cascade do |t|
+    t.string "brand"
+    t.string "origin"
+    t.string "name"
+    t.string "fuel"
+    t.integer "whels_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

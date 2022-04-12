fcb388e40b3eb05e7d0372847f24ef85d44697b2
HAROLD ADRIAN <harold.bolanos@syncromsp.com>
2022-04-11 19:15:25 -0500

    Add Routes

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'hola', to: 'welcome#hello'
+
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/bienvenido', to: 'home#index'

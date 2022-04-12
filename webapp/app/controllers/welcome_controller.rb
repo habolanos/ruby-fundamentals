3e3dd3dbe4aa650b36b006ed3bd4ce79b5fb4ddd
HAROLD ADRIAN <harold.bolanos@syncromsp.com>
2022-04-11 20:57:39 -0500

    rails credentials:edit

--- webapp/app/controllers/welcome_controller.rb
+++ webapp/app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
     def hello
+        @hello = Rails.application.credentials.hello
+        @otherthing = Rails.application.credentials.otherthing
         @variable = 'Soy '+Pet.first.name + ' un  '+Pet.first.breed
     end
-
 end

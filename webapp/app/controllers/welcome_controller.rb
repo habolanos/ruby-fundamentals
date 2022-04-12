0c3cff0485ad8eb2fac3057e710d86ec44f5e3bf
HAROLD ADRIAN <harold.bolanos@syncromsp.com>
2022-04-11 20:32:41 -0500

    Add Table as a model db

--- webapp/app/controllers/welcome_controller.rb
+++ webapp/app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
     def hello
-        
+        @variable = 'Soy '+Pet.first.name + ' un  '+Pet.first.breed
     end
 
 end
--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_051530) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_011520) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

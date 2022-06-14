cd272b86eb9a4a3749f518b978798ebbdbb573c0
Harold Adrian <harold.bolanos@syncromsp.com>
2022-06-14 10:03:08 -0500

    add notes

--- piloto-organizador/app/controllers/tasks/notes_controller.rb
+++ piloto-organizador/app/controllers/tasks/notes_controller.rb
@@ -1,6 +1,15 @@
 class Tasks::NotesController < ApplicationController
     before_action :set_task
     def create
+       @note = @task.notes.new(note_parms)
+       @note.user = current_user
+       @note.save
+    end
+
+    private
+
+    def note_parms
+        params.require(:note).permit(:body)
     end
 
     def set_task
